a0c5b3cc12b5c00c2616f51171e6472baf3d6af3
Hemant Kumar Yadav <hemantyadav8006@gmail.com>
2025-09-22 18:56:14 +0530

    basic printing code in cpp

---
+++ Basics/CppBasics.cpp
@@ -0,0 +1,18 @@
+// Generic skaleton of c++ code
+#include <iostream>
+using namespace std;
+
+void printing()
+{
+    cout << "Hello, Hemant here!" << endl; // printing the string
+    int x;
+    cout << "Enter the Value: ";
+    cin >> x; // input an int value
+    cout << "You have entered: " << x << endl;
+}
+
+int main()
+{
+    // printing();
+    return 0;
+}
