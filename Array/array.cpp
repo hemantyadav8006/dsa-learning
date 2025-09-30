8ac2bf5dfa50de412827c299a6c41767985482d1
Hemant Kumar Yadav <hemantyadav8006@gmail.com>
2025-09-30 17:11:35 +0530

    implement second largest element function and update main to display its value

--- Array/array.cpp
+++ Array/array.cpp
@@ -16,6 +16,17 @@ int largestElementInArray(int arr[], int n)
 
 int secondLargestElementInArray(int arr[], int n)
 {
+    sort(arr, arr + n);
+    int largest = arr[n - 1];
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] != largest)
+        {
+            return arr[i];
+        }
+    }
+    return -1;
 }
 
 int secondSmallestElementInArray(int arr[], int n)
@@ -27,6 +38,8 @@ int main()
     int n = 5;
     int arr[n] = {1, 5, 2, 6, 3};
     // int value = largestElementInArray(arr, n);
+    int value = secondLargestElementInArray(arr, n);
     // cout << "Largest Value of the Given Array is: " << value << endl;
+    cout << "Second Largest Value of the Given Array is: " << value << endl;
     return 0;
 }
